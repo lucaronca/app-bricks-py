cbad701b6b7602fe0f202ff0a023f6c33678f907
Luca Ronca <luca.ronca07@gmail.com>
2026-02-04 15:00:57 +0100

    Fix provider checking in cloud_llm

--- src/arduino/app_bricks/cloud_llm/models.py
+++ src/arduino/app_bricks/cloud_llm/models.py
@@ -2,16 +2,16 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-from enum import Enum
+from enum import StrEnum
 
 
-class CloudModel(str, Enum):
+class CloudModel(StrEnum):
     ANTHROPIC_CLAUDE = "claude-sonnet-4-5"  # https://platform.claude.com/docs/en/about-claude/models/overview#latest-models-comparison
     OPENAI_GPT = "gpt-5-mini"  # https://platform.openai.com/docs/models
     GOOGLE_GEMINI = "gemini-2.5-flash"  # https://ai.google.dev/gemini-api/docs/models
 
 
-class CloudModelProvider(str, Enum):
+class CloudModelProvider(StrEnum):
     ANTHROPIC = "anthropic"
     OPENAI = "openai"
     GOOGLE = "google"
